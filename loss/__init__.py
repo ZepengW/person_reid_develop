e7767b9468df9ce1dd72a617ad924441e458581b
OliverCk <oliver_ck@outlook.com>
2021-10-24 14:57:27 +0800

    update loss function load method

--- loss/__init__.py
+++ loss/__init__.py
@@ -1,7 +1,9 @@
 import logging
 from .triplet_loss import TripletLoss, WeightedRegularizedTriplet, CrossEntropyLabelSmooth
 import torch
+import torch.nn as nn
 __factory_loss = {
+    'cross_entropy': nn.CrossEntropyLoss,
     'xent_label_smooth': CrossEntropyLabelSmooth,
     'triplet': TripletLoss,
     'wrt': WeightedRegularizedTriplet
@@ -12,15 +14,22 @@ def make_loss(loss_cfg: dict, **params):
     loss_func_l = []
     loss_weight_l = []
     loss_name_l = []
-    for key in loss_cfg.keys():
+    loss_input = []
+    for key in loss_cfg.get('use_loss',['cross_entropy']):
         if not key in __factory_loss.keys():
             logging.warning(f'Can Not Find Loss Function : {key}')
             continue
         loss_params = loss_cfg[key]
-        params.update(loss_params)
-        loss_func_l.append(__factory_loss[key](**params))
+        params_initial = loss_cfg[key].get('params_initial', [])
+        ## get initial param from params
+        params_input = dict()
+        for param_name in params_initial:
+            if param_name in params.keys():
+                params_input[param_name] = params[param_name]
+        loss_func_l.append(__factory_loss[key](**params_input))
         loss_weight_l.append(loss_params.get('weight',1.0))
         loss_name_l.append(key)
+        loss_input.append(loss_params.get('except_input'))
 
     def loss_func(inputs, feats, targets):
         '''
@@ -34,15 +43,22 @@ def make_loss(loss_cfg: dict, **params):
         loss_value_l = []
         loss_name = []
         for i, loss in enumerate(loss_func_l):
-            if isinstance(inputs,list):
-                loss_value = torch.tensor(0.0).to(targets.device)
-                for input in inputs:
-                    loss_value += loss(inputs=input,feats=feats,targets=targets)
-                loss_value = loss_value / len(inputs)
-            else:
-                loss_value = loss(inputs=inputs, feats=feats, targets=targets)
-            if isinstance(loss_value,tuple):    # triplet loss return 3-tuple
-                loss_value = loss_value[0]
+            if loss_input[i] == 'score':
+                if isinstance(inputs, list):
+                    loss_value = torch.tensor(0.0).to(targets.device)
+                    for input in inputs:
+                        loss_value += loss(input, targets)
+                    loss_value = loss_value / len(inputs)
+                else:
+                    loss_value = loss(inputs=inputs, targets=targets)
+            elif loss_input[i] == 'feature':
+                if isinstance(feats, list):
+                    loss_value = torch.tensor(0.0).to(targets.device)
+                    for feat in feats:
+                        loss_value += loss(feat, targets)
+                    loss_value = loss_value / len(inputs)
+                else:
+                    loss_value = loss(feats=feats, targets=targets)
             total_loss += loss_weight_l[i] * loss_value
             loss_value_l.append(float(loss_value.cpu()))
             loss_name.append(loss_name_l[i])
