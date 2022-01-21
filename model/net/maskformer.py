0700a102abf152d5c340ed5767e72d727b0f51e3
OliverCk <oliver_ck@outlook.com>
2022-01-21 22:16:42 +0800

    maskformer2 Network use sementic segmentation information to guide the attention operation in transformer

--- model/net/maskformer.py
+++ model/net/maskformer.py
@@ -5,6 +5,7 @@ use semantics mask guided transformer
 import torch.nn as nn
 
 from model.net.backbones.vit_pytorch_transreid import TransReID
+from model.net.backbones.vit_pytorch_mask import TransReIDMask
 from functools import partial
 import copy
 import torch
@@ -131,6 +132,128 @@ class MaskFormer(nn.Module):
         img_m = img_m.permute([0, 3, 1, 2])
 
         features = self.base(img_m, cam_label=camera_id)
+
+        # global branch
+        b1_feat = self.b1(features)  # [64, 129, 768]
+        global_feat = b1_feat[:, 0]
+
+        # JPM branch
+        feature_length = features.size(1) - 1
+        patch_length = feature_length // self.divide_length
+        token = features[:, 0:1]
+
+        if self.rearrange:
+            x = shuffle_unit(features, self.shift_num, self.shuffle_groups)
+        else:
+            x = features[:, 1:]
+        # lf_1
+        b1_local_feat = x[:, :patch_length]
+        b1_local_feat = self.b2(torch.cat((token, b1_local_feat), dim=1))
+        local_feat_1 = b1_local_feat[:, 0]
+
+        # lf_2
+        b2_local_feat = x[:, patch_length:patch_length * 2]
+        b2_local_feat = self.b2(torch.cat((token, b2_local_feat), dim=1))
+        local_feat_2 = b2_local_feat[:, 0]
+
+        # lf_3
+        b3_local_feat = x[:, patch_length * 2:patch_length * 3]
+        b3_local_feat = self.b2(torch.cat((token, b3_local_feat), dim=1))
+        local_feat_3 = b3_local_feat[:, 0]
+
+        # lf_4
+        b4_local_feat = x[:, patch_length * 3:patch_length * 4]
+        b4_local_feat = self.b2(torch.cat((token, b4_local_feat), dim=1))
+        local_feat_4 = b4_local_feat[:, 0]
+
+        feat = self.bottleneck(global_feat)
+
+        local_feat_1_bn = self.bottleneck_1(local_feat_1)
+        local_feat_2_bn = self.bottleneck_2(local_feat_2)
+        local_feat_3_bn = self.bottleneck_3(local_feat_3)
+        local_feat_4_bn = self.bottleneck_4(local_feat_4)
+
+        if self.training:
+            cls_score = self.classifier(feat)
+            cls_score_1 = self.classifier_1(local_feat_1_bn)
+            cls_score_2 = self.classifier_2(local_feat_2_bn)
+            cls_score_3 = self.classifier_3(local_feat_3_bn)
+            cls_score_4 = self.classifier_4(local_feat_4_bn)
+            return [cls_score, cls_score_1, cls_score_2, cls_score_3, cls_score_4], \
+                   [global_feat, local_feat_1, local_feat_2, local_feat_3, local_feat_4]  # global feature for triplet loss
+        else:
+            return torch.cat(
+                [global_feat, local_feat_1 / 4, local_feat_2 / 4, local_feat_3 / 4, local_feat_4 / 4], dim=1)
+
+
+
+class MaskFormer2(nn.Module):
+    def __init__(self, num_classes, num_camera,
+                 vit_pretrained_path=None, img_size=224, stride_size=12,
+                 drop_path_rate=0.1, drop_rate=0.0, attn_drop_rate=0.0,
+                 sie_xishu=3.0, shuffle_groups=2, shift_num=5,
+                 divide_length=4, rearrange=True, inplanes = 768,
+                 **kwargs
+                 ):
+        super().__init__()
+        self.base = TransReIDMask(
+            img_size=img_size, patch_size=16, stride_size=stride_size, embed_dim=768, depth=12, num_heads=12,
+            mlp_ratio=4, qkv_bias=True, camera=num_camera, drop_path_rate=drop_path_rate, drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
+            norm_layer=partial(nn.LayerNorm, eps=1e-6), sie_xishu=sie_xishu, local_feature=True)
+        if not vit_pretrained_path is None:
+            self.base.load_param(vit_pretrained_path)
+        block = self.base.blocks[-1]
+        layer_norm = self.base.norm
+        self.b1 = nn.Sequential(
+            copy.deepcopy(block),
+            copy.deepcopy(layer_norm)
+        )
+        self.b2 = nn.Sequential(
+            copy.deepcopy(block),
+            copy.deepcopy(layer_norm)
+        )
+        self.in_planes = inplanes
+        self.num_classes = num_classes
+        self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier.apply(weights_init_classifier)
+        self.classifier_1 = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier_1.apply(weights_init_classifier)
+        self.classifier_2 = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier_2.apply(weights_init_classifier)
+        self.classifier_3 = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier_3.apply(weights_init_classifier)
+        self.classifier_4 = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier_4.apply(weights_init_classifier)
+        self.bottleneck = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck.bias.requires_grad_(False)
+        self.bottleneck.apply(weights_init_kaiming)
+        self.bottleneck_1 = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck_1.bias.requires_grad_(False)
+        self.bottleneck_1.apply(weights_init_kaiming)
+        self.bottleneck_2 = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck_2.bias.requires_grad_(False)
+        self.bottleneck_2.apply(weights_init_kaiming)
+        self.bottleneck_3 = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck_3.bias.requires_grad_(False)
+        self.bottleneck_3.apply(weights_init_kaiming)
+        self.bottleneck_4 = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck_4.bias.requires_grad_(False)
+        self.bottleneck_4.apply(weights_init_kaiming)
+
+        self.shuffle_groups = shuffle_groups
+        logging.info('using shuffle_groups size:{}'.format(self.shuffle_groups))
+        self.shift_num = shift_num
+        logging.info('using shift_num size:{}'.format(self.shift_num))
+        self.divide_length = divide_length
+        logging.info('using divide_length size:{}'.format(self.divide_length))
+        self.rearrange = rearrange
+
+    def forward(self, img, ss, camera_id=None):
+        '''
+        split img with semantic segmentation mask
+        '''
+
+        features = self.base(img, ss, cam_label=camera_id)
 
         # global branch
         b1_feat = self.b1(features)  # [64, 129, 768]
